3cf7ac4cf022d279164f0e896a604b61b7f50c80
Ananth <28anantha0202@gmail.com>
2026-06-15 18:21:57 +0530

    Added files for cicd

---
+++ tests/test_main.py
@@ -0,0 +1,25 @@
+from src.main import add, subtract, multiply, divide
+import pytest
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+    assert subtract(0, 1) == -1
+    assert subtract(-1, -1) == 0
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, 5) == 0
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(-4, 2) == -2
+    with pytest.raises(ValueError):
+        divide(5, 0)
+
+
